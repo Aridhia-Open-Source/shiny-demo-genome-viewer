39d80d62def8e3fdfb71029a48006036d1157982
Harry Peaker <HarryPeaker@Aridhia.net>
2019-08-20 11:59:41 +0100

    add Rproj and tidy up

--- workspace/mini-apps/genome_browser/server.r
+++ workspace/mini-apps/genome_browser/server.r
@@ -1,22 +1,3 @@
-library(shiny)
-library(ggvis)
-library(dplyr)
-source("ggcircos_helpers.r")
-
-use_donors <- c("DO32875", "DO32878", "DO32900", "DO33091", "DO33256", "DO33336", "DO33344", "DO33368",
-                "DO33376", "DO33392", "DO33400", "DO33408", "DO33480", "DO33512", "DO33528", "DO33544",
-                "DO33552", "DO33600", "DO33632", "DO33656", "DO33984", "DO34240", "DO34264", "DO34288",
-                "DO34312", "DO34368", "DO34376", "DO34432", "DO34448", "DO34600", "DO34608", "DO34656",
-                "DO34696", "DO34736", "DO34785", "DO34793", "DO34801", "DO34809", "DO34817", "DO34849",
-                "DO34905", "DO34961")
-
-donors <- xap.read_table("donors_clean")  %>% filter(icgc_donor_id %in% use_donors)
-snp <- xap.read_table("snp_clean") %>% filter(icgc_donor_id %in% use_donors, chromosome != "MT")
-struct <- xap.read_table("struct_clean")  %>% filter(icgc_donor_id %in% use_donors) 
-cnv <- xap.read_table("cnv_clean") %>% filter(icgc_donor_id %in% use_donors)
-genes <- xap.read_table("genes")
-top_genes <- xap.read_table("icgc_top_genes")
-clinvar <- xap.read_table("clinvar_variant_summary")
 
 
 snp_genes <- snp %>%

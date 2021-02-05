b2a12376bc4865f734609ad2c8d3c671e99c2b7f
annadomenech <anna.domenech@aridhia.com>
2021-02-05 14:40:30 +0000

    fixed README and help tab

--- README.md
+++ README.md
@@ -2,13 +2,29 @@
 
 # GENOME VIEWER
 
-This RShiny mini-app allows you to visualise whole genomes usig Circos. Circos is a visualisation tool developed for displaying and analysing data from genome sequencing.
+This RShiny mini-app allows you to visualise whole genomes usig Circos, which is a visualisation tool developed for displaying and analysing data from genome sequencing.
 
 This app uses data taken from https://dcc.icgc.org/. Specifically, the data comes from a project led by Sean M. Grimmond and Andrew V. Biankin, who gathered samples of pancreatic adenocarcinoma tumours from various donors. The data from this specific project can be found
 https://dcc.icgc.org/repository/icgc/release_19/Projects/PACA-AU.
 
 
-For more information, you can read the blog about this app at http://www.aridhia.com/blog/beauty-in-simplicity-visualising-large-scale-genomic-data/.   
+For more information, you can read the blog about this app at http://www.aridhia.com/blog/beauty-in-simplicity-visualising-large-scale-genomic-data/.
+
+### About the Genome Viewer mini-app
+
+The **Circos-style plot** has a number of layers (from outwards to inwards):
+
+1. Size of chromosomes, moving clockwise, 1 to 22, X and Y; each one displayed in a different colour.
+2. Line representation of the number of mutations within a chromosome. When expanded (by clicking in the previous layer), more details are revealed.
+3. Scatter plots showing the counts of transcripts. The more outer the point is, the more transcripts are affected by the mutation at that coordinate. The points are both colour and shape coded, green and blue points showing the insertions and deletions, whereas red points show single point mutations. Those SNPs marked by a cross indicate mutations with an associated ClinVar record.
+4. Inner ribbons representing portions of the genome that have been restructured relative to the Hg19 reference genome, these are known as structural variants.
+
+This is an interactive plot, if you hover over the different elements, more information will appear.
+
+On the right-side of the screen you will see:
+1. A **table** displaying the most common mutations amongst the donors
+2. A **slider input** to switch between different donors
+4. The **SNP Consequence checkbox** used to filter mutations
 
 #### Checkout and run
 
--- code/app_info.R
+++ code/app_info.R
@@ -6,29 +6,29 @@
 app_info <- function() {
   
   div(id = "app_info", class = "collapse out",
-  h4("Introduction"),
   p("This RShiny mini-app allows you to visualise whole genomes usig Circos. This app uses data taken from the", tags$a(href = "https://dcc.icgc.org/", "ICGC"),
     ". Specifically, the data comes from a project led by Sean M. Grimmond and Andrew V. Biankin, who gathered samples of pancreatic adenocarcinoma tumours from various donors", 
     tags$a(href = "https://dcc.icgc.org/repository/icgc/release_19/Projects/PACA-AU", "(PACA-AU)") , "."), 
-  h4("How to use the App"),
-  p("Firstly, if the", strong(" plot"), " isn't quite the optimal size, locate the edge at the very bottom right corner of the plot, which will allow you to drag the size in and out."),
-  p("The Circos-style plot shows a number of layers, starting from the outer layer representing colour and size distinguished chromsomes, moving clockwise from chromosome 1 to 22, 
-    then X and Y. Inside the chromosomes ring, you can see a line representation of copy number mutations. When expanded (by clicking a chromosome), more details on the precise 
-    copy number at specific locations is revealed. Hover the bars for further details and external resources."),
-  p("Moving inwards, counts of transcripts are shown as scatter plots. The more outer the point, the more transcripts affected by the mutation at that coordinate. The points are 
+  h3("How to use the App"),
+  p("The ", strong("Circos-style plot"), " has a number of layers (from outwards to inwards):"),
+  tags$ol(
+    tags$li("Shows the size of chromosomes, from chromosome 1 to 22, X and Y (moving clockwise) each one in a different color"),
+    tags$li("A line representation of the number of mutations within a chromosome. When expanded (by clicking the previous layer), more details are revealed."),
+    tags$li("Scatter plots showing the counts of transcripts. The more outer the point is, the more transcripts affected by the mutation at that coordinate. The points are 
     both colour and shape coded, green and blue points showing the insertions and deletions, whereas red points show single point mutations. Those SNPs marked by a cross indicate 
-    mutations with an associated ClinVar record. Hovering on a point provides more information, and reveals further links to view the genes or transcripts in Ensembl. 
-    The tooltips presented when hovering reveal further links,  or save interesting genes in the app as a list to refer back to."),
-  p("Finally the inner ribbons represent portions of the genome that have been restructured relative to the Hg19 reference genome, known as structural variants."),
+    mutations with an associated ClinVar record."),
+    tags$li("The inner ribbons represent portions of the genome that have been restructured relative to the Hg19 reference genome, these are known as structural variants.")
+  ),
+  p("This is an interactive plot, if you hover over the different elements, more information will appear, as well as furhter links to view the genes or transcripts in Ensembl and ClinVar."),
+  p("You can change the size of the plot using the edge at the bottom right."),
   p("On the right-side of the screen you will see:"),
   tags$ol(
-    tags$li("A ", strong("table"), " displaying the most common mutations amongs the donors"),
+    tags$li("A ", strong("table"), " displaying the most common mutations amongst the donors"),
     tags$li("A ", strong("slinder input "), "to switch between different donors"),
     tags$li("The ", strong('SNP Consequence'), "checkbox group can be used to filter out mutations, by default we've selected only mutations in or around 
     the coding regions of the genome.")
   ),
-  br(),
-  h4("Video"),
+  h3("Video"),
   tags$video(src="genome-viewer.mp4", type = "video/mp4", frameborder = "0", height = "350", controls = NA),
   p(class = "nb", "NB: This mini-app is provided for demonstration purposes only, is unsupported and is utilised at user's risk. If you plan to use this mini-app
     to inform your study, please review the code and ensure you are comfortable with the calculations made before proceeding.")

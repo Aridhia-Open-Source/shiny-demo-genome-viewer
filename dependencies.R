b61e71bef6455029ad22a24e5df15f6cb326381f
hpeaker <harry_peaker@hotmail.com>
2021-03-09 19:05:35 +0000

    install only if required

--- dependencies.R
+++ dependencies.R
@@ -1,5 +1,15 @@
+package_install <- function(x, ...) {
+  for (i in x) {
+    # Check if package is installed
+    if (!require(i, character.only = TRUE)){
+      # If the package could not be loaded then install it
+      install.packages(i, ...)
+    }
+  }
+}
+
 
 # Packages needed to run the app
-install.packages("shiny")
-install.packages("ggvis")
-install.packages("dplyr")
+packages <- c("shiny", "ggvis", "dplyr")
+
+package_install(packages)
